fab0798a8a8db58e50af12a925e49acc10fdf1df
sq-96 <shengqian@uchicago.edu>
2023-04-20 20:01:54 -0500

    update

--- code/run_WhiteBlood_analysis_M.sh
+++ code/run_WhiteBlood_analysis_M.sh
@@ -4,14 +4,14 @@ for WEIGHT in WholeBlood
 do 
 	TRAIT_FILE="/project2/xinhe/shengqian/cTWAS/cTWAS_analysis/data/UKBB/ukb-d-30000_irnt.vcf.gz"
 	LD_DIR="/project2/mstephens/wcrouse/UKB_LDR_0.1"
-	WEIGHT_FILE="/project2/xinhe/shengqian/cTWAS/cTWAS_analysis/data/mqtl/GTEx_Whole_Blood_Methylation.db"
+	WEIGHT_FILE="/project2/xinhe/shengqian/cTWAS/cTWAS_analysis/data/mqtl/WholeBlood_enet.db"
 	CONFIG_FILE="/project2/xinhe/shengqian/cTWAS/cTWAS_analysis/code/ctwas_config_b38.R"
 	OUTNAME_E="WhiteBlood_"$WEIGHT"_expr"
 	OUTNAME="WhiteBlood_"$WEIGHT"_ctwas"
-	OUTDIR="/project2/xinhe/shengqian/cTWAS/cTWAS_analysis/data/WhiteBlood_M/"$WEIGHT
+	OUTDIR="/project2/xinhe/shengqian/cTWAS/cTWAS_analysis/data/WhiteBlood_M_enet/"$WEIGHT
 	job_name="WhiteBlood_"$WEIGHT
-	job_out="/project2/xinhe/shengqian/cTWAS/cTWAS_analysis/code/WhiteBlood_M_out/"$job_name".out"
-	job_err="/project2/xinhe/shengqian/cTWAS/cTWAS_analysis/code/WhiteBlood_M_out/"$job_name".err"
+	job_out="/project2/xinhe/shengqian/cTWAS/cTWAS_analysis/code/WhiteBlood_M_ener_out/"$job_name".out"
+	job_err="/project2/xinhe/shengqian/cTWAS/cTWAS_analysis/code/WhiteBlood_M_enet_out/"$job_name".err"
   
 	FINAL_FILE=$OUTDIR"/"$OUTNAME".susieIrss.txt"
 	if [ ! -f $FINAL_FILE ]
